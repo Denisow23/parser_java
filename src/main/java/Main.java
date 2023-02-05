bb96172b37432512d065bc846e91a0641f0e0397
Your Name <danilka2330@gmail.com>
2023-02-05 15:58:43 +0300

    repository was clean. Delete comments.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,36 +8,36 @@ import java.util.Objects;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Document doc = HTMLParser.connection("https://skillbox-java.github.io/");//код сайта
+        Document doc = HTMLParser.connection("https://skillbox-java.github.io/");
         ArrayList<Line> lines = HTMLParser.parseLines(doc);
-        HTMLParser.parseStations(lines, doc);//информация о названиях станций и линий, номера линий, информация о переходах
+        HTMLParser.parseStations(lines, doc);
 
         ArrayList<String> JSONPathes = new ArrayList<>();
         ArrayList<String> CSVPathes = new ArrayList<>();
-        Objects.requireNonNull(Recursive.list("data")).forEach(path -> {//рекурсивный обход папок
+        Objects.requireNonNull(Recursive.list("data")).forEach(path -> {
             if (path.toPath().toString().regionMatches(path.toPath().toString().lastIndexOf('.'),
-                    ".json", 0, 5)) {JSONPathes.add(path.toPath().toString());}//если файл заканчивается на .json
+                    ".json", 0, 5)) {JSONPathes.add(path.toPath().toString());}
             else CSVPathes.add(path.toPath().toString());//иначе
         });
 
-        JSONPathes.forEach(path -> {//лист файлов .json
+        JSONPathes.forEach(path -> {
             try {
-                JSONParser.parse(path, lines);//парсинг файлов
+                JSONParser.parse(path, lines);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         });
 
         CSVPathes.forEach(path -> {
-            CSVPapser.parse(path, lines);//парсинг файлов .csv из листа с адресами файлов
+            CSVPapser.parse(path, lines);
         });
 
-        JSONLines jsonLines = new JSONLines(lines);//класс для создания 1-го .json файла, похожего на map.json
+        JSONLines jsonLines = new JSONLines(lines);
 
         ObjectMapper mapper = new ObjectMapper();
         File output = new File("src/main/resources/export.json");
-        mapper.writerWithDefaultPrettyPrinter().writeValue(output, jsonLines);//запись 1-го файла
+        mapper.writerWithDefaultPrettyPrinter().writeValue(output, jsonLines);
 
-        JSONStations.writeFile(lines);//создание и запись 2-го файла
+        JSONStations.writeFile(lines);
     }
 }
